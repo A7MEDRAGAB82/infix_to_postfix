a802c868b5c7dd50f169cbd5c9820e06fa2a253e
Ahmed_Ragab(Alexandria university) <166864012+A7MEDRAGAB82@users.noreply.github.com>
2025-04-20 23:51:42 +0200

    first change

--- Data_task/src/Main.java
+++ Data_task/src/Main.java
@@ -7,10 +7,12 @@ import static algo.Infix2Postfix.post_eval;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("this is infix to postfix : ");
         String infix = "24+1.2*2";
         String postfix = in2post(infix);
         System.out.println("Infix: " + infix);
         System.out.println("Postfix: " + postfix);
+        System.out.println("this is postfix evaluation : ");
         System.out.println("Result: " + post_eval(postfix));
         }
     }
